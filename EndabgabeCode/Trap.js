640e72bdc13e35b89b49ad58a9ad7510dfaabda1
SamuelKasper <64090949+SamuelKasper@users.noreply.github.com>
2021-06-20 18:20:48 +0200

    trap size to 2

--- EndabgabeCode/Trap.js
+++ EndabgabeCode/Trap.js
@@ -4,12 +4,12 @@ var Dodge;
     var f = FudgeCore;
     class Trap extends Dodge.QuadNode {
         constructor(x, y) {
-            super("trap", new f.Vector2(x, y), new f.Vector2(3, 3));
+            super("trap", new f.Vector2(x, y), new f.Vector2(2, 2));
             this.textureTrap = new f.TextureImage("./Images/Trap.png");
             this.materialTrap = new f.Material("materialTrap", f.ShaderTexture, new f.CoatTextured(null, this.textureTrap));
             this.textureSpike = new f.TextureImage("./Images/Spike.png");
             this.materialSpike = new f.Material("materialTrap", f.ShaderTexture, new f.CoatTextured(null, this.textureSpike));
-            this.textureNode = new Dodge.QuadNode("textureNode", new f.Vector2(0, 0), new f.Vector2(3, 3));
+            this.textureNode = new Dodge.QuadNode("textureNode", new f.Vector2(0, 0), new f.Vector2(2, 2));
             this.activateTrap = () => {
                 //add new image material
                 this.textureNode.getComponent(f.ComponentMaterial).material = this.materialSpike;
--- EndabgabeCode/Trap.ts
+++ EndabgabeCode/Trap.ts
@@ -5,10 +5,10 @@ namespace Dodge {
         private materialTrap: f.Material = new f.Material("materialTrap", f.ShaderTexture, new f.CoatTextured(null, this.textureTrap));
         private textureSpike: f.TextureImage = new f.TextureImage("./Images/Spike.png");
         private materialSpike: f.Material = new f.Material("materialTrap", f.ShaderTexture, new f.CoatTextured(null, this.textureSpike));
-        private textureNode: QuadNode = new QuadNode("textureNode", new f.Vector2(0, 0), new f.Vector2(3, 3));
+        private textureNode: QuadNode = new QuadNode("textureNode", new f.Vector2(0, 0), new f.Vector2(2, 2));
 
         constructor(x: number, y: number) {
-            super("trap", new f.Vector2(x, y), new f.Vector2(3, 3));
+            super("trap", new f.Vector2(x, y), new f.Vector2(2, 2));
             //remove material given from quadnote
             this.removeComponent(this.getComponent(f.ComponentMaterial));
             //add new image material
